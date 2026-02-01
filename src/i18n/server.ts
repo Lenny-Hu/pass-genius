88367c321ae2f2db06cbfa51c37ad39f1b69297f
lenny hu <a97251292@gmail.com>
2026-02-01 04:38:55 +0000

    还是报错啊

--- src/i18n/server.ts
+++
@@ -1,6 +0,0 @@
-import { createI18nServer } from 'next-international/server';
- 
-export const { getI18n, getScopedI18n, getCurrentLocale } = createI18nServer({
-  zh: () => import('@/locales/zh'),
-  en: () => import('@/locales/en'),
-});
